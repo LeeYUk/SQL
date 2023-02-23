8e729d1396f386c4036757506f9f57143a19f4ed
LeeYUk <118726154+LeeYUk@users.noreply.github.com>
2023-02-23 18:48:42 +0900

    day18

---
+++ Script.sql
@@ -0,0 +1,23 @@
+CREATE TABLE "HR"."member2" 
+   (	"ID" VARCHAR2(100), 
+	"PW" VARCHAR2(100), 
+	"NAME" VARCHAR2(100), 
+	"TEL" VARCHAR2(100)
+   ) 
+
+CREATE TABLE HR.bbs2(
+	id varchar2(100),
+	title varchar2(100),
+	content varchar2(100),
+	writer varchar2(100)
+)
+
+
+CREATE TABLE "HR"."PRODUCT222" 
+   (	"ID" VARCHAR2(100), 
+	"NAME" VARCHAR2(100), 
+	"CONTENT" VARCHAR2(100), 
+	"PRICE" NUMBER(38,0)
+   )
+  
+INSERT INTO "member" VALUES('park','1234','park','011')
